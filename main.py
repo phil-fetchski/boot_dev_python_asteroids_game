d371ea3a4ad79c410223bdbe754aabec77d09e1f
Phil Schools <philip.schools87@gmail.com>
2026-05-16 16:02:10 -0400

    CH3 Complete - Moving into CH4: Asteroids

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
     
 
@@ -19,9 +22,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for un_drawn in drawable:
+            un_drawn.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
